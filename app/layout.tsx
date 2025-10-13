a1429d081e7d7055bad6f6fad555ee1ca80e37ec
Ram Murmu <runashInc@outlook.com>
2025-10-13 21:52:55 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          
+          <Banner />
           <Providers>
             <Suspense fallback={null}>{children}</Suspense>
             {/* Cookie consent dialog */}
