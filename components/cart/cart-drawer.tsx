f328e8acb85874a5c4a3b94bd4a8b5abae4f274f
Ram Murmu <runashInc@outlook.com>
2025-12-19 15:12:48 +0530

    Update cart-drawer.tsx

--- components/cart/cart-drawer.tsx
+++ components/cart/cart-drawer.tsx
@@ -14,7 +14,7 @@ import CouponInput from "./coupon-input"
 import ShippingOptions from "./shipping-options"
 
 export default function CartDrawer() {
-  const { state, toggleCart, getItemCount, clearCart } = useCart()
+  const { state, toggleCart, getItemCount, clearCart, proceedCheckout } = useCart()
   const { cart, isOpen, totals } = state
 
   const itemCount = getItemCount()
@@ -93,7 +93,8 @@ export default function CartDrawer() {
 
             {/* Checkout Button */}
             <div className="space-y-2">
-              <Button className="w-full bg-gradient-to-r from-orange-600 to-yellow-500 hover:from-orange-700 hover:to-yellow-600 text-white">
+              <Button className="w-full bg-gradient-to-r from-orange-600 to-yellow-500 hover:from-orange-700 hover:to-yellow-600 text-white"
+                onClick={proceedCheckout}>
                 <CreditCard className="h-4 w-4 mr-2" />
                 Proceed to Checkout
               </Button>
